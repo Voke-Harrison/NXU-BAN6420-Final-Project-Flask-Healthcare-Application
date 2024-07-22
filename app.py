bff735ba308408aa1ca87573a080405e52a93532
Voke-Harrison <vokeeda@gmail.com>
2024-07-22 22:54:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,15 @@
 # Voke Harrison Edafejimue
 # Learner ID - 143304
 
+
 # import necessary libraries
 from flask import Flask, render_template, request, url_for, redirect
+from pymongo import MongoClient
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-import certifi
+import csv
 
 
-ca = certifi.where()
 
 # Flask constructor
 app = Flask(__name__)
@@ -27,11 +28,26 @@ try:
 except Exception as e:
     print(e)
 
-
 # Create a database
 db = client.flask_db
 # Create a collection/table
 users = db.users
+
+
+class user:
+    cursor = users.find()
+    # Extract the data from the cursor
+    data = [doc for doc in cursor]
+    # Specify the file path for the CSV file
+    file_path = r"./data.csv"
+    # Specify the field names for the CSVfile
+    field_names = list(data[0].keys())
+    # Write the data to the CSV file
+    with open(file_path, "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=field_names)
+        writer.writeheader()
+        for doc in data:
+            writer.writerow(doc)
 
 
 @app.route("/", methods=('GET', 'POST'))
@@ -52,6 +68,15 @@ def index():
         return redirect(url_for('index'))
     all_users = users.find()
     return render_template('index.html', users=all_users)
+
+
+@app.route("/export", methods=('GET', 'POST'))
+def export():
+    if request.method == 'POST':
+        user()
+        return redirect(url_for('export'))
+    all_users = users.find()
+    return render_template('export.html', users=all_users)
 
 
 if __name__ == '__main__':
